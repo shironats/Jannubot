0dad277a00f775f60998005020f8f5faa3966345
shironats <66661623+shironats@users.noreply.github.com>
2020-09-17 23:07:50 +0700

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -2,126 +2,136 @@ import discord
 from discord.ext import commands, tasks
 import io
 import aiohttp
-import datetime
 import random
 
 bot = commands.Bot(command_prefix='}')
 bot.remove_command('help')
-imageCounter = 5
-#target_channel = ID here #botfest
-#target_channel = ID here #general
-#target_channel = ID here #bot_test
-#raid_channel = ID here #raids
-#danchou = '<@&ID here>'
-#officers = '<@&ID here>'
-#haipa = '<@!ID here>'
-#victim = :kmr:
+downImageCounter = 6
+upImageCounter = 2
+botfest = ID Here #botfest
+general = ID Here #general
+botTest = ID Here #bot_test
+vc_channel = ID Here #vc-for-mutes
+raid_channel = ID Here #raids
+danchou = '<@&ID Here>'
+officers = '<@&ID Here>'
+haipa = '<@!ID Here>'
+self = '<@!ID Here>'
+nadekoBOT = ID Here
 
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
     await bot.change_presence(activity=discord.Game(name='}help'))
 
+@bot.event
+async def on_message(message):
+    nadeko = bot.get_user(nadekoBOT)
+    if message.author == bot.user:
+        return
+    elif message.author == nadeko:
+        if message.content.startswith('}down'):
+            if message.content.endswith('mutes'):
+                downSpamBot.start(vc_channel)
+            elif message.content.endswith('general'):
+                downSpamBot.start(general)
+            else:
+                downSpamBot.start(botfest)
+        else:
+            await bot.process_commands(message)
+
+    await bot.process_commands(message)
+
 @bot.command(pass_context = True)
 async def help(ctx):
     embed = discord.Embed(colour = discord.Colour.orange())
     embed.set_author(name='Help')
-    embed.add_field(name='}down', value='Sends a random "Buff is down" image', inline=False)
-    embed.add_field(name='}up', value='Sets timer for next buff reactivation', inline=False)
-    embed.add_field(name='}checkImages', value='Check }down images', inline=False)
+    embed.add_field(name='}down', value='Spams random "Buff is down" images', inline=False)
+    embed.add_field(name='}up', value='Sends a random "Buff is up" image', inline=False)
+    embed.add_field(name='}checkImages', value='Check all images', inline=False)
+    embed.add_field(name='Source Code', value='https://github.com/shironats/Buffbot/blob/Update-14/07/DiscordBot.py', inline=False)
     await ctx.send(embed=embed)
 
 @bot.command()
 async def down(ctx):
     """Sends a random 'Buff is down' image"""
-    downReminder.cancel()
-    ITSDOWN.cancel()
-    link = mySwitch(random.randint(0,imageCounter-1))
-    async with aiohttp.ClientSession() as session:
-        async with session.get(link) as resp:
-            if resp.status != 200:
-                return await ctx.send('Could not download file...')
-            data = io.BytesIO(await resp.read())
-            if link[27:] == ".jpg":
-                await ctx.send(file=discord.File(data, 'BUFFDOWN.jpg'))
-            elif link[27:] == ".png":
-                await ctx.send(file=discord.File(data, 'BUFFDOWN.png'))
-            elif link[27:] == ".gif":
-                await ctx.send(file=discord.File(data, 'BUFFDOWN.gif'))
-            else:
-                await ctx.send("ERROR")
+    downSpam.start(ctx)
+
+@bot.command()
+async def downTest(ctx, imgID: int):
+    """Function for testing"""
+    link = downSwitch(imgID)
+    await sendPics(ctx, link, False)
 
 @bot.command()
 async def up(ctx):
     """Sets timer for next buff reactivation"""
-    await ctx.send('Buff has been reactivated on {0}'.format(datetime.datetime.now()))
-    downReminder.start()
+    downSpam.cancel()
+    downSpamBot.cancel()
+    link = upSwitch(random.randint(0,upImageCounter-1))
+    await sendPics(ctx, link, False)
 
 @bot.command()
 async def checkImages(ctx):
     """Check }down images"""
-    for iCounter in range(imageCounter):
-        link = mySwitch(iCounter)
-        async with aiohttp.ClientSession() as session:
-            async with session.get(link) as resp:
-                if resp.status != 200:
-                    return await ctx.send('Could not download file...')
-                data = io.BytesIO(await resp.read())
-                if link[27:] == ".jpg":
-                    await ctx.send(file=discord.File(data, 'BUFFDOWN.jpg'))
-                elif link[27:] == ".png":
-                    await ctx.send(file=discord.File(data, 'BUFFDOWN.png'))
-                elif link[27:] == ".gif":
-                    await ctx.send(file=discord.File(data, 'BUFFDOWN.gif'))
-                else:
-                    await ctx.send("ERROR")
+    for iCounter in range(downImageCounter):
+        link = downSwitch(iCounter)
+        await sendPics(ctx, link, False)
+    for iCounter in range(upImageCounter):
+        link = upSwitch(iCounter)
+        await sendPics(ctx, link, False)
 
-#@tasks.loop(minutes=58, hours=71)
 @tasks.loop(seconds=2)
-async def downReminder():
-    message_channel = bot.get_channel(target_channel)
-    if downReminder.current_loop == 0:
-        await message_channel.send('Reminder has been set for {0}'.format(datetime.datetime.now()+datetime.timedelta(hours = 71, minutes = 58)))
-    elif downReminder.current_loop == 1:
-        await message_channel.send('Buffs will go down in 2 minutes.')
-        ITSDOWN.start()
-    else:
-        await message_channel.send('ERROR!! downReminder not yet terminated!!')
+async def downSpam(ctx):
+    link = downSwitch(random.randint(0,downImageCounter-1))
+    await sendPics(ctx, link, True, downSpam.current_loop)
 
-#@tasks.loop(minutes=2)
 @tasks.loop(seconds=2)
-async def ITSDOWN():
-    message_channel = bot.get_channel(target_channel)
-    if ITSDOWN.current_loop == 0:
-        downReminder.cancel()
-    else:
-        link = mySwitch(random.randint(0,imageCounter-1))
-        async with aiohttp.ClientSession() as session:
-            async with session.get(link) as resp:
-                if resp.status != 200:
-                    return await ctx.send('Could not download file...')
-                data = io.BytesIO(await resp.read())
-                if link[27:] == ".jpg":
-                    await message_channel.send('%s, Please type in (or maybe spam) "}up" to continue...' %victim, file=discord.File(data, 'BUFFDOWN.jpg'))
-                elif link[27:] == ".png":
-                    await message_channel.send('%s, Please type in (or maybe spam) "}up" to continue...' %victim, file=discord.File(data, 'BUFFDOWN.png'))
-                elif link[27:] == ".gif":
-                    await message_channel.send('%s, Please type in (or maybe spam) "}up" to continue...' %victim, file=discord.File(data, 'BUFFDOWN.gif'))
-                else:
-                    await ctx.send("ERROR")
-        
-def mySwitch(argument):
+async def downSpamBot(target_channel):
+    link = downSwitch(random.randint(0,downImageCounter-1))
+    msgChannel = bot.get_channel(target_channel)
+    await sendPics(msgChannel, link, True, downSpamBot.current_loop)
+
+def downSwitch(argument):
     switcher = {
         0: 'https://i.imgur.com/Ns9DV9R.jpg',
         1: 'https://i.imgur.com/EGDsKdv.png',
         2: 'https://i.imgur.com/SDRmkhD.jpg',
         3: 'https://i.imgur.com/uJp4D7C.jpg',
-        4: 'https://i.imgur.com/agYqbci.jpg'
+        4: 'https://i.imgur.com/agYqbci.jpg',
+        5: 'https://i.imgur.com/H7bos6u.gif'
         }
     return switcher.get(argument, "Invalid pick")
 
-def stopAllTimers():
-    downReminder.cancel()
-    ITSDOWN.cancel()
+def upSwitch(argument):
+    switcher = {
+        0: 'https://i.imgur.com/1FaipNk.png',
+        1: 'https://i.imgur.com/kEOah4r.jpg'
+        }
+    return switcher.get(argument, "Invalid pick")
 
-bot.run('token here')
+async def sendPics(ctx, imglink, withText, loopNum = 0):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(imglink) as resp:
+            if resp.status != 200:
+                return await ctx.send('Could not download file...')
+            data = io.BytesIO(await resp.read())
+            if imglink.endswith(".jpg"):
+                if withText == True:
+                    await ctx.send('%s or %s, Please reactivate crew buffs, this is reminder number %i' %(danchou,officers,loopNum), file=discord.File(data, 'img.jpg'))
+                else:
+                    await ctx.send(file=discord.File(data, 'img.jpg'))
+            elif imglink.endswith(".png"):
+                if withText == True:
+                    await ctx.send('%s or %s, Please reactivate crew buffs, this is reminder number %i' %(danchou,officers,loopNum), file=discord.File(data, 'img.png'))
+                else:
+                    await ctx.send(file=discord.File(data, 'img.png'))
+            elif imglink.endswith(".gif"):
+                if withText == True:
+                    await ctx.send('%s or %s, Please reactivate crew buffs, this is reminder number %i' %(danchou,officers,loopNum), file=discord.File(data, 'img.gif'))
+                else:
+                    await ctx.send(file=discord.File(data, 'img.gif'))
+            else:
+                await ctx.send("ERROR")
+
+bot.run('ID Here')
