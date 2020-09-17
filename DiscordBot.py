6a65bd10da120a12eb583dd2efa2e590a7038422
shironats <66661623+shironats@users.noreply.github.com>
2020-09-17 23:13:20 +0700

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -13,9 +13,9 @@ vc_channel = ID Here #vc-for-mutes
 raid_channel = ID Here #raids
 danchou = '<@&ID Here>'
 officers = '<@&ID Here>'
-haipa = '<@!ID Here>'
-self = '<@!ID Here>'
-sol = '<@!ID Here>'
+haipa = ID Here
+self = ID Here
+sol = ID Here
 nadekoBOT = ID Here
 
 #============== BOT EVENTS ================================
@@ -42,6 +42,10 @@ async def on_message(message):
     if message.content.lower().endswith('aaaaa'):
         await sendSinglePic(message.channel, 'https://i.imgur.com/8n1zvzR.jpg')
 
+#send jangkrik
+    if message.content.lower().find('cricket cricket') != -1:
+        await sendSinglePic(message.channel, 'https://i.imgur.com/ors2jnC.gif')
+    
     await bot.process_commands(message)
 
 #============== BOT COMMANDS ==============================
@@ -53,8 +57,10 @@ async def help(ctx):
     embed.add_field(name='}up', value='Sends a random "Buff is up" image', inline=False)
     embed.add_field(name='}checkImages', value='Check all images', inline=False)
     embed.add_field(name='}truck [@ someone]', value='Sends a truck after that person', inline=False)
-    embed.add_field(name='Other Features', value='Send 5 "a"s', inline=False)
-    embed.add_field(name='Source Code', value='https://github.com/shironats/Buffbot/blob/Update-16/07/DiscordBot.py', inline=False)
+    embed.add_field(name='}bonk [@ someone]', value='Bonks that person', inline=False)
+    embed.add_field(name='}riot', value='Time to RIOT!!', inline=False)
+    embed.add_field(name='Other Features', value='Send 5 "a"s\nSend "cricket cricket"', inline=False)
+    embed.add_field(name='Source Code', value='https://github.com/shironats/Buffbot/blob/Update-29/07/DiscordBot.py', inline=False)
     await ctx.send(embed=embed)
 
 @bot.command()
@@ -63,9 +69,16 @@ async def down(ctx):
     downSpam.start(ctx)
 
 @bot.command()
-async def downTest(ctx, imgID: int):
+async def test(ctx, func: str, imgID: int):
     """Function for testing"""
-    link = downImages[imgID]
+    if (func == 'down'):
+        link = downImages[imgID]
+    elif (func == 'up'):
+        link = upImages[imgID]
+    elif (func == 'truck'):
+        link = truckImages[imgID]
+    else:
+        link = bonkImages[imgID]
     await sendSinglePic(ctx, link)
 
 @bot.command()
@@ -97,28 +110,36 @@ async def truck(ctx, member: discord.Member):
     """Sends a truck over"""
     link = truckImages[random.randint(0,len(truckImages)-1)]
     embed = discord.Embed(colour = discord.Colour.red())
-    embed.set_footer(text='{0.nick}, {1.nick} sends their regards.'.format(member, ctx.message.author))
+    embed.set_footer(text='{0.display_name}, {1.display_name} sends their regards.'.format(member, ctx.message.author))
     embed.set_image(url='attachment://img%s' %(link[27:]))
     await sendSinglePic(ctx, link, embed)
 
 @bot.command()
 async def bonk(ctx, member: discord.Member):
     """Bonks a member"""
-    link = bonkImages[random.randint(0,len(bonkImages))]
+    link = bonkImages[random.randint(0,len(bonkImages)-1)]
     embed = discord.Embed(colour = discord.Colour.blurple())
-    embed.set_footer(text='{0.nick} has been BONKED by {1.nick} for being bad.'.format(member, ctx.message.author))
+    if (member == bot.get_user(self)):
+        embed.set_footer(text='{0.display_name} has been BONKED by {1.display_name} for being bad.'.format(ctx.message.author, member))
+    else:
+        embed.set_footer(text='{0.display_name} has been BONKED by {1.display_name} for being bad.'.format(member, ctx.message.author))
     embed.set_image(url='attachment://img%s' %(link[27:]))
     await sendSinglePic(ctx, link, embed)
+
+@bot.command()
+async def riot(ctx):
+    """Time to RIOT!!"""
+    await sendSinglePic(ctx, 'https://i.imgur.com/fyG8NZk.png')
 
 #============== BOT LOOPS =============================
 @tasks.loop(seconds=2)
 async def downSpam(ctx):
-    link = downSwitch(random.randint(0,downImageCounter-1))
+    link = downImages[random.randint(0,len(downImages)-1)]
     await sendPics(ctx, link, True, downSpam.current_loop)
 
 @tasks.loop(seconds=2)
 async def downSpamBot(target_channel):
-    link = downSwitch(random.randint(0,downImageCounter-1))
+    link = downImages[random.randint(0,len(downImages)-1)]
     msgChannel = bot.get_channel(target_channel)
     await sendPics(msgChannel, link, True, downSpamBot.current_loop)
 
@@ -128,7 +149,8 @@ downImages = ['https://i.imgur.com/Ns9DV9R.jpg',
               'https://i.imgur.com/SDRmkhD.jpg',
               'https://i.imgur.com/uJp4D7C.jpg',
               'https://i.imgur.com/agYqbci.jpg',
-              'https://i.imgur.com/H7bos6u.gif']
+              'https://i.imgur.com/H7bos6u.gif',
+              'https://i.imgur.com/NngbAJi.png']
 
 upImages = ['https://i.imgur.com/1FaipNk.png',
             'https://i.imgur.com/kEOah4r.jpg']
@@ -138,7 +160,8 @@ truckImages = ['https://i.imgur.com/qYhwU3x.gif',
 
 bonkImages = ['https://i.imgur.com/3e1P8oW.gif',
               'https://i.imgur.com/fIWm3Hh.gif',
-              'https://i.imgur.com/fmZY9dV.gif']
+              'https://i.imgur.com/fmZY9dV.gif',
+              'https://i.imgur.com/VkYz0vr.gif']
 
 #============== ASYNC FUNCTIONS =======================
 async def sendPics(ctx, imglink, withText, loopNum = 0):
